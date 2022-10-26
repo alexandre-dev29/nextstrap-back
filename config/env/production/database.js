977aad77ad6527975ca997f04f31c2c2170298da
alexandre-dev29 <axel.business29@gmail.com>
2022-10-26 14:08:08 +0200

    update config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: process.env.HOST,
+      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
